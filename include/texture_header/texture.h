96593c45d1faa3b9d0c7af1952f806fb399312e1
mrizaln <mrizaln2000@gmail.com>
2023-01-06 13:45:02 +0700

    Rename stb directory

--- include/texture_header/texture.h
+++ include/texture_header/texture.h
@@ -3,7 +3,7 @@
 
 #include <glad/glad.h>  // include glad to get all the required OpenGL headers
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image/stb_image.h>
+#include <stb/stb_image.h>
 
 #include <iostream>
 #include <limits>
